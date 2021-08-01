784565b0d0f74c9b89fa4e20ff885191b5c896dd
Ayush Budhwani <ayushbudhwani@Ayushs-MacBook-Air.local>
2021-08-01 12:20:26 -0400

    Added a protected no match route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home.js'
 import Signup from './components/auth/Signup.js'
 import List from './components/List.js'
 import Add from './components/Add.js'
+import NoPathMatch from './components/NoPathMatch'
 
 const ProtectedRoute = (props) => {
   return(
@@ -12,19 +13,28 @@ const ProtectedRoute = (props) => {
         (<Redirect to={{pathname:'/login'}}></Redirect>):
         (<props.component {...data}></props.component>)}></Route>
     )
-  }
+}
+
+const LoginSignUpRollBackRoute = (props) => {
+  return(
+        <Route path={props.path} render={data=> localStorage.getItem('user_id') === null || localStorage.getItem('user_id') === "NOACTIVEUSER" ? 
+        (<props.component {...data}></props.component>):
+        (<Redirect to={{pathname: '/home'}}></Redirect>)}></Route>
+    )
+}
 
 const App = () => {
 
   return (
     <div className="App">
       <Switch>
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/signup" component={Signup} />
+        <LoginSignUpRollBackRoute exact path="/login" component={Login} />
+        <LoginSignUpRollBackRoute exact path="/signup" component={Signup} />
         <ProtectedRoute exact path="/home" component={Home}/>
         <ProtectedRoute exact path="/" component={Home}/>
         <ProtectedRoute exact path="/list" component={List} />
         <ProtectedRoute exact path="/add" component={Add} />
+        <Route component={NoPathMatch}/>
       </Switch>
     </div>
   );
